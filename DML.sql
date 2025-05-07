4d2de2dc05fec05e58afb3afa193ca66267789ea
jinliuli <jinliuli0305@gmail.com>
2025-05-08 00:41:51 +0900

    core: DB User테이블 Role 컬럼 값 수정

--- DML.sql
+++ DML.sql
@@ -1,17 +1,27 @@
 --Users 1명
 INSERT INTO Users (
     id, userName, loginId, password, phoneNumber, role,
-    companyName, workType, teamName, jobType, isActive
+    companyName, teamName, jobType, isActive
 ) VALUES (
     seqUsers.NEXTVAL,       -- 자동 증가 ID
     '홍길동',                 -- 이름
     'hong',                 -- 로그인 ID
     '123',                  -- 비밀번호 (암호화 전 예시)
     '010-1234-5678',        -- 연락처
-    'TEAM_LEADER',          -- 권한
-    '한진설비',               -- 업체명
-    '배관',                  -- 공종명
-    '1팀',                   -- 팀명
-    '용접사',                 -- 직종명
+    'MEMBER',          -- 권한
+    '삼진설비',               -- 업체명
+    '배관 1팀',                   -- 팀명
+    '배관',                 -- 직종명
     'Y'                     -- 재직 여부
 );
+select * from users;
+
+UPDATE Users
+SET role = 'ROLE_MEMBER'
+WHERE loginId = 'hong';
+
+UPDATE Users
+SET role = 'ROLE_ADMIN'
+WHERE loginId = 'tiger';
+
+commit;
